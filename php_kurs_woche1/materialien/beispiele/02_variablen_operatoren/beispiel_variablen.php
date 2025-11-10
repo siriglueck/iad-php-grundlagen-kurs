2a0dbd3fc301eb2ce7b787279aa9d9e69b93f9f0
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-10 13:07:18 +0000

    Beispiel Variablen beendet

--- php_kurs_woche1/materialien/beispiele/02_variablen_operatoren/beispiel_variablen.php
+++ php_kurs_woche1/materialien/beispiele/02_variablen_operatoren/beispiel_variablen.php
@@ -1,5 +1,32 @@
 <?php
 declare(strict_types=1);
+$name = 'Siri';
+$age = 10;
+$lucky = 777;
+$sum = $age + $lucky;
+/*
+Arithmetische Operatoren:
++   Addition
+-   Subtraktion
+*   Multiplikation
+/   Division
+%   Modulo (Rest einer Division)
+
+Verkettungsoperator (Konkatenator):
+.   Verkettung von Strings (nicht + wie in JS)
+
+Vergleichsoperatoren:
+<   Kleiner als
+>   Größer als
+<=  Kleiner gleich
+>=  Größer gleich
+==  Gleich (Wertgleichheit)
+!=  Ungleich
+<>  Ungleich
+=== Identisch (Wert- und Typgleichheit)
+!== Nicht identisch
+
+*/
 ?>
 <!doctype html>
 <html lang="de">
@@ -12,7 +39,9 @@ declare(strict_types=1);
 <body>
   <header><h1>Variablen & Operatoren</h1></header>
   <main class="container">
-    
+    <p> Hallo <?= htmlspecialchars($name) ?>, du bist <?= $age ?> Jahre alt</p>
+    <p> und dein Glückszahl ist <?= $lucky ?>. </p>
+    <p> Die Summe ist <?= $sum ?>. </p>    
   </main>
 </body>
 </html>
