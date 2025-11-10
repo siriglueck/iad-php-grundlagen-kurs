b217f7f668ff1718c113a5d72bcce360c6822642
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-10 11:57:14 +0000

    Test-HTML-Datei angelegt, erstes Beispiel erfolgreich absolviert

--- php_kurs_woche1/materialien/beispiele/01_hallo_welt/index.php
+++ php_kurs_woche1/materialien/beispiele/01_hallo_welt/index.php
@@ -7,9 +7,14 @@
   <link rel="stylesheet" href="../../style/style.css">
 </head>
 <body>
-  <header><h1>Hallo PHP 8.4 👋</h1></header>
+  <header><h1>Hallo PHP 8.3 👋</h1></header>
   <main class="container">
-    
+    <p>
+      <?php echo "Hallo PHP 8.3!" ?>>
+    </p>
+    <p>
+      Datum: <?php echo date("d.m.y"); ?>>
+    </p>
   </main>
 </body>
 </html>
