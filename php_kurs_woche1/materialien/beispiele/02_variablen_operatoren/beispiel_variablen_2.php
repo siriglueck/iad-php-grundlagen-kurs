6e59ec8c92e8fb1064e652f8fc087c44f2570dab
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-11 08:51:02 +0000

    Zweites Variablen-Beispiel hinzugefügt und ausgeführt

---
+++ php_kurs_woche1/materialien/beispiele/02_variablen_operatoren/beispiel_variablen_2.php
@@ -0,0 +1,39 @@
+<?php
+    
+    error_reporting(E_ALL);
+    ini_set('display_errors', true);
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variablen Fortsetzung</title>
+    <link rel="stylesheet" href="../../style/style.css">
+</head>
+<body>
+    <?php 
+        $einString = 'Ich bin ein String'; // Datentyp: String
+        echo '<p>Der Inhalt der Variable ist:  . $einString . </p>';
+        echo '<p>Der Inhalt der Variable ist: ' . $einString . '</p>';
+        echo "<p>Der Inhalt der Variable ist:  $einString. </p>\r\n";
+        echo '<p>Eine mit Komma getrennte Ausgabe mit mehreren Zeichenketten: ', $einString, ' ist aus einer Variable.', '</p>';
+
+        $preisZiege = '0.5 Kamele';
+        $menge = 5;
+        $gesamtPreis = $preisZiege * $menge; // Achtung: PHP wandelt den String in eine Zahl um
+        echo "<p>Der Gesamtpreis für $menge Ziegen beträgt $gesamtPreis Kamele.</p>";
+
+        // ### Konstanten ###
+        // klassische Variante
+        define('MEINE_KONSTANTE', 'Ich bin eine Konstante');
+        define('SEK_TAG', 24 * 60 *60); // Sekunden pro Tag
+        // neue Variante seit PHP 5.5
+        const MIN_TAG = 24 * 60;
+
+        echo '<p> Ein Tag hat ', MIN_TAG,' Minuten. bzw. ', SEK_TAG,' Sekunden.</p>';
+    ?>
+</body>
+</html>
