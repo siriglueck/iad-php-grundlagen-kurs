d7a04283489698c807c6c1e5ca994b2fdfdb3704
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-17 12:20:48 +0000

    Beispiel: Mit JSON arbeiten hinzugefügt

--- php_kurs_woche2/materialien/beispiele_notizmanager/03_json/beispiel_json_read.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/03_json/beispiel_json_read.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
-
+$path = __DIR__ . '/notes.json';
+$notes = is_file($path) ? json_decode( (string)file_get_contents($path), true) : [];
 ?>
 <!doctype html>
 <html lang="de">
@@ -13,7 +14,16 @@ declare(strict_types=1);
 <body>
 <header><h1>JSON lesen</h1></header>
 <main class="container">
-  
+  <?php if( $notes ): ?>
+    <?php foreach( $notes as $n ): ?>
+      <article class="post">
+        <h2><?= htmlspecialchars($n['title']) ?></h2>
+        <p><?= nl2br(htmlspecialchars($n['content'])) ?></p>
+      </article>  
+    <?php endforeach; ?>
+  <?php else: ?>
+    <p class="alert">Keine Einträge gefunden. Bitte zuerst <code><a href="beispiel_json_write.php">beispiel_json_write.php</a></code> ausführen</p>
+  <?php endif; ?>
 </main>
 </body>
 </html>
--- php_kurs_woche2/materialien/beispiele_notizmanager/03_json/beispiel_json_write.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/03_json/beispiel_json_write.php
@@ -1,3 +1,10 @@
 <?php
 declare(strict_types=1);
 
+$notes = [
+    ['title' => 'Test', 'content' => 'Hallo JSON!']
+];
+
+file_put_contents(__DIR__ . '/notes.json', json_encode($notes, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+echo 'notes.json geschrieben' ;
+
