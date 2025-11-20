cdfccac4ac3f1a6cb38797b7455ffaf35544ad1b
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-20 13:13:26 +0000

    TBC : Funktion updateNote() hinzugefügt und Bearbeiten-Funktionalität hinzugefügt

--- php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
+++ php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
@@ -26,3 +26,14 @@ function findNote(PDO $pdo, int $id): ?object {
     $row = $stmt->fetch();
     return $row ?: null;
 }
+
+function updateNote(PDO $pdo, int $id, string $title, string $content, ?int $categoryId = null):void {
+    $stmt = $pdo->prepare('UPDATE notes SET title=:t, content=:c, category_id=:cat
+    WHERE id=:id');
+    $stmt->execute([
+        ':t' => $title,
+        ':c' => $content,
+        ':cat' => $categoryId,
+        ':id' => $id
+    ]);   
+}   
---
+++ php_kurs_woche3/projekt_notizmanager_db/public/update.php
@@ -0,0 +1,20 @@
+<?php
+    declare(strict_types=1);
+    //! die folgendern 2 Zeilen in der Produktiv-Variante löschen!
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    require_once __DIR__ . '/../inc/db-connect.php';
+    require_once __DIR__ . '/../inc/functions.php';
+
+    $id = (int)($_POST['id'] ?? 0);
+    $title = trim($_POST['title'] ?? '');
+    $content = trim($_POST['content'] ?? '');
+    $cat = $_POST['category_id'] ?? '';
+    $catID = ($cat === '' ? null : (int)$cat);
+
+    if ($id && $title !== '' && $content !== '') {
+        updateNote($pdo, $id, $title, $content, $catId);
+    }
+
+    header('Location: index.php');
